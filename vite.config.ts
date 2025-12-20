9febf6eccc175035ef1044b5fa09c08df7d93122
Stanley T Lee <cloudtardis@users.noreply.github.com>
2025-12-20 22:17:38 +1100

    changed from /snake/ to just / , hopefully will sovle the problem when we switch to sub-domain

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import path from 'path'
 
 export default defineConfig({
-  base: '/snake/',
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
